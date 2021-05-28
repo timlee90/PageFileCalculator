fda5e41ac3823886122e237a5dcbce595f4c3347
timlee90 <68327199+timlee90@users.noreply.github.com>
2021-05-28 13:55:00 +0100

    Imported from known working copy of script.

---
+++ PageFile.ps1
@@ -0,0 +1,32 @@
+write-host 
+write-host "==================================="
+write-host "Page File Calculator"
+write-host -foreground Red "TIP: CTRL+C to close calculator"
+write-host "==================================="
+write-host ""
+$ram = read-host "Enter your memory amount"
+$minpage = 1024*$ram*1.5
+$maxpage = 1024*$ram*3
+Write-host
+Write-host -foreground Magenta "Your Min pagefile should be set to; " -nonewline
+write-host -foreground red "$minpage."
+write-host -foreground Magenta "Your Max pagefile should be set to; " -nonewline
+write-host -foreground red "$maxpage."
+Write-Host
+
+$message  = 'This is a Choice'
+$question = 'Do you want to do another calculation? [Y/N]'
+
+$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+$decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
+if ($decision -eq 0)
+{
+    & "/Users/timjoynson/Documents/Languages/Powershell/PageFileCalculator/pagefile.ps1"
+}
+else
+{
+    exit 
+}
