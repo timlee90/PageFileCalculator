9cd7a34e6f207ad02882e82d2773d318b807239c
timlee90 <68327199+timlee90@users.noreply.github.com>
2022-05-03 13:35:05 +0100

    Update Master.ps1
    Renamed original script to master.ps1, following new convention. 
    Added MB/GB results, added restart line that will report user that script is restarting and clearing terminal screen.
    Added disk size required for dedicated pagefile disk.

--- PageFile.ps1
+++ PageFile.ps1
@@ -5,16 +5,27 @@ write-host -foreground Red "TIP: CTRL+C to close calculator"
 write-host "==================================="
 write-host ""
 $ram = read-host "Enter your memory amount"
+
 $minpage = 1024*$ram*1.5
-$maxpage = 1024*$ram*3
+$minpageGB = $minpage/1024
+
+$maxpage = 1024*$ram*4
+$maxpageGB = $maxpage/1024
+
+$DiskSize = $maxpage+1024
+$disksizeGB = $disksize/1024
+
 Write-host
 Write-host -foreground Magenta "Your Min pagefile should be set to; " -nonewline
-write-host -foreground red "$minpage."
+write-host -foreground red $minpage "MB" / $minpageGB "GB"
 write-host -foreground Magenta "Your Max pagefile should be set to; " -nonewline
-write-host -foreground red "$maxpage."
+write-host -foreground red $maxpage "MB"/ $maxpageGB "GB"
 Write-Host
+Write-host -foreground Magenta "Page Disk Size Required; " -nonewline 
+write-host -foreground red $DiskSize "MB" / $disksizeGB "GB"
+write-host
 
-$message  = 'This is a Choice'
+$message  = 'This is a Choice, choose wisely'
 $question = 'Do you want to do another calculation? [Y/N]'
 
 $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
@@ -24,10 +35,17 @@ $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentL
 $decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
 if ($decision -eq 0)
 {
-    & "$PSScriptRoot/pagefile.ps1"
+    write-host
+    write-host -ForegroundColor red "Restarting calculator, clearing terminal screen in 5 seconds."
+    Start-Sleep 5
+    clear
+    & "$PSScriptRoot/Master.ps1"
 }
 else
 {
+    write-host
+    write-host -ForegroundColor red "You have chosen to quit, closing Powershell in 5 seconds."
+    Start-Sleep 5
     exit 
 }
 
